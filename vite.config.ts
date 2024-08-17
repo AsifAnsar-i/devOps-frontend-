3fa349164097d45356864716e3641e9809c29537
Asif Ansari <moaisfansari1999@gmail.com>
2024-08-17 14:05:44 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-});
+  preview:{
+    host:true,
+    port:5173
+  },
+  define: {
+    'process.env': process.env
+  }
+})
