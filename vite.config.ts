9dfc6bbb12588daad759f4133143f337ce0268a3
Asif Ansari <moaisfansari1999@gmail.com>
2024-08-17 14:02:08 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://backend:7000', // Make sure this is correct
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-  preview:{
-    host:true,
-    port:5173
-  },
-  define: {
-    'process.env': process.env
-  }
-})
+});
