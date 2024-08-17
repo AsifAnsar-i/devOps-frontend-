c30e409df636a45e51505c819b2c9b99ccf65650
Asif Ansari <moaisfansari1999@gmail.com>
2024-08-17 15:42:38 +0530

    commit0

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://backend-w9xd.onrender.com', // Make sure this is correct
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
   preview:{
     host:true,
     port:5173
